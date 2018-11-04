90b5b7d5a888f2e897382ca36ed6a8515f1dbc3a
Martin Maugeais <mrtnmgs@gmail.com>
2018-11-04 16:20:47 -0500

    handle audio.play() status to avoid DOMexception

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -11,30 +11,46 @@ class Player extends Component {
   static propTypes = {
     url: PropTypes.string.isRequired
   };
-  
+
   constructor(props) {
-  super(props);
-    this.state = { label: "Sound is off" };
+    super(props);
+    this.state = {
+      label: "Sound is off",
+      canplay: false
+    };
     this.audio = new Audio(this.props.url);
   }
 
-  componentDidMount() {
-    this.updateLabel()
-  }
-
-  updateLabel = () => {
-    const label = this.audio.paused ? 'Sound is off' : 'Sound is on';
-    this.setState({ label });
-  }
-
   togglePlay = () => {
-    // console.log(this.audio.paused);
-    this.audio.paused ? this.audio.play() : this.audio.pause();
-    this.updateLabel();
-  }
+    if (this.audio.paused) {
+      // audio was paused, we're going to start playing
+      // first set label as loading
+      this.setState({ label: "Loading..." });
+      // set event listener to update state.canplay when ready
+      this.audio.addEventListener("canplay", () => this.setState({ canplay: true }));
+      // play the sound and store the Promise in a var so we can check it when we want to pause
+      // (otherwise we'll get 'DOMException: The play() request was interrupted' on pause)
+      this.playPromise = this.audio.play();
+      // finally update label
+      this.playPromise.then(() => {
+        this.setState({ label: "Sound is on" })
+      });
+    } else {
+      if (this.playPromise !== undefined) {
+        // the promise must be resolved before we try to pause
+        this.playPromise
+        .then(_ => {
+          // pause
+          this.audio.pause();
+          // finally update label
+          this.setState({ label: "Sound is off" });
+          })
+      }
+    }
+  };
 
   render() {
-    return <Button clickAction={this.togglePlay} label={this.state.label} />
+    return <Button clickAction={this.togglePlay} label={this.state.label} />;
   }
 }
 
